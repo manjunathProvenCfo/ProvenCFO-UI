3667ad6adf43b0ac3ccfc475414936aca54deb45
jai5manjunath <jai5manjunath@gmail.com>
2021-08-17 13:41:16 +0530

    PRO-500 Call Xero API to get outstanding amount from Demo Organization and populates in home page

---
+++ Comman/XeroInstance.cs
@@ -0,0 +1,39 @@
+﻿using Proven.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Xero.NetStandard.OAuth2.Token;
+
+namespace ProvenCfoUI.Comman
+{
+    public sealed class XeroInstance
+    {
+        private static XeroInstance _instance = null;
+        private static readonly object _syncObject = new object();        
+        private XeroInstance()
+        {
+            
+        }
+        public static XeroInstance Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncObject)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new XeroInstance();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+        public IXeroToken XeroToken { get; set; }
+        public XeroService XeroService { get; set; }
+    }
+}
