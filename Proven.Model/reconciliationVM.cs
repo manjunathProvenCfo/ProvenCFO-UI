67512be476f8713e6c0a26e3058af0e2b7c51ab6
jai5manjunath <jai5manjunath@gmail.com>
2021-09-16 14:33:21 +0530

    PRO-587 econciliation table Who and Description column should be wrapped

--- Proven.Model/reconciliationVM.cs
+++ Proven.Model/reconciliationVM.cs
@@ -18,9 +18,11 @@ namespace Proven.Model
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime date { get; set; }
         public String description { get; set; }
+        public String description_display { get { return description != null && description.Length > 20 ? description.Substring(0, 20) + "..." : description; } }
         public string gl_account { get; set; }
         public string reconciled { get; set; }
         public String reference { get; set; }
+        public string reference_display { get { return reference != null && reference.Length > 20 ? reference.Substring(0, 20) + "...": reference; } }
         public string rule { get; set; }
         //public string type { get; set; }
         public String AgencyID { get; set; }
