259994a57b132e048bdc3b8da2217045caa89e5a
jefferson <jefferson.nunn@boyneresorts.com>
2021-10-11 16:08:25 -0700

    PRO-751

--- assets/js/reports.js
+++ assets/js/reports.js
@@ -62,14 +62,14 @@ $(function () {
         }
         else {
             if (year == currentYear) {
-                $divReportPeriodYearEnd.show();
+                $divReportPeriodYearEnd.hide();
 
                 for (var i = 0; i < (12 - currentMonth + 1); i++) {
 
                     if (i == 0)
-                        $divReportPeriodQuarters.filter("[data-report-period='Q4']").show();
+                        $divReportPeriodQuarters.filter("[data-report-period='Q4']").hide();
                     else if (i == 3)
-                        $divReportPeriodQuarters.filter("[data-report-period='Q3']").show();
+                        $divReportPeriodQuarters.filter("[data-report-period='Q3']").hide();
                     else if (i == 6)
                         $divReportPeriodQuarters.filter("[data-report-period='Q2']").hide();
                     else if (i == 9)
