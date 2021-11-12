4beb658b0e87af1355f4175cef3fa9e693e2c574
jai5manjunath <jai5manjunath@gmail.com>
2021-11-12 12:56:25 +0530

    PRO-943 Research and implementation about the Dashboard for third party

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Proven.Model;
+using Proven.Service;
+using ProvenCfoUI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,32 @@ namespace ProvenCfoUI.Controllers
     public class DashboardController : Controller
     {
         // GET: Dashboard
-        public ActionResult Index()
+        public ActionResult Dashboard()
         {
-            return View();
+            ClientModel objClient = new ClientModel();
+            List<UserPreferencesVM> UserPref = (List<UserPreferencesVM>)Session["LoggedInUserPreferences"];
+            if (UserPref != null && UserPref.Count() > 0)
+            {
+                using (ClientService obj = new ClientService())
+                {
+                    var selectedAgency = UserPref.Where(x => x.PreferenceCategory == "Agency" && x.Sub_Category == "ID").FirstOrDefault();
+                    var objResult = obj.GetClientById(Convert.ToInt32(selectedAgency.PreferanceValue));
+                    if (!string.IsNullOrEmpty(objResult.DashboardURLId) && objResult.DashboardId != null && objResult.DashboardId != 0)
+                    {
+                        return View(objResult);
+                    }
+                    else
+                    {
+                        return RedirectToAction("DashboardError", "Dashboard");
+                    }
+                }
+            }
+            return RedirectToAction("DashboardError", "Dashboard");
+        }
+
+        public ActionResult DashboardError()
+        {
+            return PartialView("DashboardError");
         }
     }
 }
