e6addb6f5af45d8ad50af0e87c4e2879782468ee
DESKTOP-EJ8GUVP\Manjunath k <manjunath@provencfo.com>
2022-02-21 17:24:51 +0530

    PRO-1379 Reconciliation chat time - Is the chat time format in ISD, PST and UTC.

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -147,7 +147,9 @@ var LoadAllComments = function (ReconciliationComments) {
             var dhtml = CommentHtmls.datehtml.replace('{id}', aDates.date.replace('-', '')).replace('{innerText}', datestring);
             $channelMessages.append(dhtml);
             $.each(aDates.comments, function (index, acomments) {
-                var time = getCurrentTime(new Date(acomments.createdDate));
+               
+                var UTCdate = getUTCDateTime(new Date(acomments.createdDateUTC));
+                var time = getCurrentTime(new Date(UTCdate));
                 var profileimgurl = acomments.commentedUserProfileImageurl;
                 var commentText = acomments.commentText;
                 var userName = acomments.commentedUserName;
@@ -210,6 +212,23 @@ function getCurrentTime(date) {
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0' + minutes : minutes;
     var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+function getUTCDateTime(date) {
+
+    var dt = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    var seconds = date.getSeconds()
+    var minutes = date.getMinutes();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = month + '/' + dt + '/' + year + ' ' + hours + ':' + minutes + ' ' + ampm + ' UTC';
     return strTime;
 }
 //$btnSendMessage.on("change", function (e) {
