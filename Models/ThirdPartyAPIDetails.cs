8d23a76c9627e4c3ade63d0d7109b2b0a38c9b07
GINILYTICS\Sahil.jarial <sahil@ginilytics.com>
2022-07-25 10:43:10 +0530

    Thirdpartyapidetails view models

---
+++ Models/ThirdPartyAPIDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProvenCfoUI.Models
+{
+    public class ThirdPartyAPIDetailsVW
+    {
+        public int Id { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string ThirdParty { get; set; }
+
+        public int Priority { get; set; }
+
+        public bool Status { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Proven.Model/ThirdPartyAPIDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProvenCfoUI.Models
+{
+
+    [Serializable]
+    public class ThirdPartyAPIDetailsVM
+    {
+        public int Id { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string ThirdParty { get; set; }
+
+        public int Priority { get; set; }
+
+        public bool Status { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+
+
+
+
+    [Serializable]
+
+    public  class  ThirdPartyAPIDetails
+    {
+
+        public List<ThirdPartyAPIDetailsVM> list { get; set; }
+   
+    }
+}
