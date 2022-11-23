1355f69e32f4499715271934e88318ef7861a83f
GINILYTICS\Sahil.jarial <sahil@ginilytics.com>
2022-11-23 12:24:52 +0530

    fixed-2290,2295,2296

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -728,9 +728,28 @@ var LoadAllComments = function (ReconciliationComments) {
 
         // Edit: to add it in the array format instead
         const commentsgroupArrays = Object.keys(dategroups).map((date) => {
+            var key = date;
+            let utcDate = dategroups[date][0].createdDateUTC;
+
+            let timeZoneOffset = new Date().getTimezoneOffset();
+            let utcServerDateTime = new Date(utcDate);
+            let utcTimeInMilliseconds = utcServerDateTime.getTime();
+            debugger;
+            var localTime;
+
+            switch ((timeZoneOffset > 0)) {
+                case true:
+                    localTime = new Date(utcTimeInMilliseconds - (timeZoneOffset * 60000));
+                    break;
+                case false:
+                    localTime = new Date(utcTimeInMilliseconds + ((-1 * timeZoneOffset) * 60000));
+                    break;
+            }
+
+            date = `${localTime.getFullYear()}-${localTime.getMonth() + 1}-${localTime.getDate()}`;
             return {
                 date,
-                comments: dategroups[date]
+                comments: dategroups[key]
             };
         });
         $.each(commentsgroupArrays, function (index, aDates) {
