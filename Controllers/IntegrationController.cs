65897885808f3bf070acd42c1315b2ca6151a50e
DESKTOP-EJ8GUVP\Manjunath k <manjunath@provencfo.com>
2023-03-14 10:41:26 +0530

    Hot fix

--- Controllers/IntegrationController.cs
+++ Controllers/IntegrationController.cs
@@ -535,7 +535,7 @@ namespace ProvenCfoUI.Controllers
         public async Task<JsonResult> GenerateAuthorizationPromptUrl(string Id, int ThirdPartyAccountingApp_ref)
         {
             string redirecturl = string.Empty;
-            string callbackurl = $"https://{ HttpContext.Request.Url.Authority}/Integration/Callback";
+            string callbackurl = $"https://{ HttpContext.Request.Url.Authority}/";            
             Session["EditingClientId"] = Id;
             switch (ThirdPartyAccountingApp_ref)
             {
