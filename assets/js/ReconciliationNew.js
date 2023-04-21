136ee43a62430ee030c3c8ed3a71558a94f58e9f
DESKTOP-EJ8GUVP\Manjunath k <manjunath@provencfo.com>
2023-04-21 18:18:38 +0530

    ht fix

--- assets/js/ReconciliationNew.js
+++ assets/js/ReconciliationNew.js
@@ -88,9 +88,9 @@ function RenderBankRule(r, r1, r3) {
                                                     </div>`;
 }
 function GLAccountsRender(r, r1, r3) {  //value,name,row
-    var dateFormated = getLocalTime(formatDateTime(r3.GlAccountModifiedDateUTC, 'DD MMM YYYY hh:mm:ss A'));
-    var select =
-        `<div class="row justify-content-between">
+    var dateFormated = formatDateTime(r3.GlAccountModifiedDateUTC, 'DD MMM YYYY hh:mm:ss A');
+    dateFormated = getLocalTime(dateFormated);
+    var select =`<div class="row justify-content-between">
                        <div class="col-auto lastmodified glACCOUNTS" id="Gltoggel" data-toggle="tooltip" data-html="true" utc="${r3.GlAccountModifiedDateUTC}" ModifiedBy="${r3.GlAccountModifiedBy}" title="No Modification yet.">
 
                             <select class="select-picker gl-account" utcdate="${dateFormated}" ModifiedBy=${r3.GlAccountModifiedBy} data-reconciliationId=${r3.id} data-selectedValue=${r} style="width:250px;">${glAccountsOpt}</select>
